8eb2053d04cf98ebb7a1e1ebe694a914a8d82408
Thomas <thomaskavi2000@gmail.com>
2026-07-12 14:48:28 -0300

    fix: angular.json appoint to prod environment

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, computed, signal } from '@angular/core';
 import { Router } from '@angular/router';
-import { environment } from '../../../environments/environment';
+import { environment } from '../../../environments/environment.production';
 import { LoginRequest, LoginResponse } from '../../models/models';
 
 const CHAVE_TOKEN = 'danbarber_token';
--- src/app/core/services/usuario.service.ts
+++ src/app/core/services/usuario.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../../environments/environment';
+import { environment } from '../../../environments/environment.production';
 
 export interface UsuarioResponse {
   id: number;
